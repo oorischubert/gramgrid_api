2db2792a0aa74fac8b19a4a040a772df3785730f
Oori Schubert <oorischubert@gmail.com>
2022-07-20 15:59:16 +0300

    web config 2

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin #for web 
+from flask_cors import cross_origin #for web 
 
 FindMistakesData = [
 {'correct':'My mother always wakes up early and goes to the gym in the morning.',
@@ -21,10 +21,9 @@ VerbData = [
 
 # creating the instance of our flask application
 app = Flask(__name__)
-CORS(app, support_credentials=True)
 
 @app.route('/fm',methods = ['GET', 'POST'])
-@cross_origin(supports_credentials=True) #web auth
+@cross_origin(maxAge = 3600) #web auth
 def downloadRoute0():
     global FindMistakesData
     if(request.method == 'GET'):
@@ -37,7 +36,7 @@ def downloadRoute0():
         return ' '
 
 @app.route('/vd',methods = ['GET', 'POST'])
-@cross_origin(supports_credentials=True) #web auth
+@cross_origin(maxAge = 3600) #web auth
 def downloadRoute1():   
     global VerbData
     if(request.method == 'GET'):
